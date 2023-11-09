166ccfac49bde81f2bfdcbaa2bb321e24178dfa9
cbowlin7 <149842895+cbowlin7@users.noreply.github.com>
2023-11-08 23:27:04 -0500

    Add files via upload

---
+++ ProblemSet2_2.java
@@ -0,0 +1,18 @@
+package problemset2_2;
+
+public class ProblemSet2_2 {
+
+    //This method prints out a heart!
+	public static void main(String[] args) {
+		System.out.println("   __     __    ");
+		System.out.println("  /XX\\   /XX\\ ");
+		System.out.println(" /XXXX\\ /XXXX\\");
+		System.out.println(" \\XXXXXXXXXXX/ ");
+		System.out.println("  \\XXXXXXXXX/  ");
+		System.out.println("   \\XXXXXXX/   ");
+		System.out.println("    \\XXXXX/    ");
+		System.out.println("     \\XXX/     ");
+		System.out.println("      \\x/      ");
+		}
+		          }
+
